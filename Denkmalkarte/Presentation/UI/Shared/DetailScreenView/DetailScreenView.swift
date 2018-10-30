226871c5143f1a1bc4b5ea32c49115dbf3df5f0a
‘Florian <fhaeusler@posteo.de>
2018-10-30 09:31:03 +0100

    delete testcode in DetailScreenView

--- Denkmalkarte/Presentation/UI/Shared/DetailScreenView/DetailScreenView.swift
+++ Denkmalkarte/Presentation/UI/Shared/DetailScreenView/DetailScreenView.swift
@@ -3,12 +3,5 @@ import UIKit
 public class DetailScreenView: UIViewController, DetailScreenViewProtocol {
 
     var presenter: DetailScreenPresenterProtocol?
-    var id: [Int]?
-
-    public override func viewDidLoad() {
-        if let test = id {
-            print(test[0])
-        }
-    }
 
 }
