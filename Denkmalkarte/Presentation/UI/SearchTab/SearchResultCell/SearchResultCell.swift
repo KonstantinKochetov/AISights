9727c59128cdb1a81e950a1f90324ee59e9de8dd
Konstantin Kochetov <s0559121@htw-berlin.de>
2018-11-05 05:18:28 +0100

    ui and swiftlint

--- Denkmalkarte/Presentation/UI/SearchTab/SearchResultCell/SearchResultCell.swift
+++ Denkmalkarte/Presentation/UI/SearchTab/SearchResultCell/SearchResultCell.swift
@@ -2,18 +2,18 @@ import UIKit
 
 class SearchResultCell: UITableViewCell {
 
-    
     @IBOutlet weak var denkmalImageView: UIImageView!
-    
+
     @IBOutlet weak var denkmalNameLabel: UILabel!
-    
+
     @IBOutlet weak var denkmalDescriptionLabel: UILabel!
-    
+
     @IBOutlet weak var denkmalDistanceLabel: UILabel!
+
     public static let identifier = "SearchResultCell"
 
     public func set(result: String) {
-
+        denkmalImageView.image = UIImage(named: "testimage")
     }
 
 }
