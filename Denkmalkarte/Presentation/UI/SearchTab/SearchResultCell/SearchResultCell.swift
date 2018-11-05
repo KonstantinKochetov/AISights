21609021910d112b96d00d7fd476540402ccae37
Konstantin Kochetov <s0559121@htw-berlin.de>
2018-11-05 05:01:32 +0100

    cell ui basic

--- Denkmalkarte/Presentation/UI/SearchTab/SearchResultCell/SearchResultCell.swift
+++ Denkmalkarte/Presentation/UI/SearchTab/SearchResultCell/SearchResultCell.swift
@@ -2,12 +2,17 @@ import UIKit
 
 class SearchResultCell: UITableViewCell {
 
-    @IBOutlet weak var titleView: UILabel!
-
+    
+    @IBOutlet weak var denkmalImageView: UIImageView!
+    
+    @IBOutlet weak var denkmalNameLabel: UILabel!
+    
+    @IBOutlet weak var denkmalDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var denkmalDistanceLabel: UILabel!
     public static let identifier = "SearchResultCell"
 
     public func set(result: String) {
-        titleView.text = result
 
     }
 
