73cbff50fba5922f64a1ae4adc2a594ec31d83f2
jlncrml <s0559811@htw-berlin.de>
2018-12-12 23:44:42 +0100

    Add Get Directions Feature

--- Denkmalkarte/Presentation/UI/Shared/DetailScreenView/DetailScreenView.swift
+++ Denkmalkarte/Presentation/UI/Shared/DetailScreenView/DetailScreenView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import MapKit
 
 public class DetailScreenView: UIViewController, DetailScreenViewProtocol {
     
@@ -6,19 +7,47 @@ public class DetailScreenView: UIViewController, DetailScreenViewProtocol {
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var distanceLabel: UILabel!
     @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var streetLabel: UILabel!
+    @IBOutlet private var textLabel: UILabel!
     
     public override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
     var presenter: DetailScreenPresenterProtocol?
+    var monument: RealmDenkmal?
+    
+    public override func viewDidLoad() {
+        setup()
+    }
     
     // MARK: Actions
     
     @IBAction func dismissButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-
+    
+    @IBAction func directionsButtonTapped(_ sender: Any) {
+        openMonumentInMaps()
+    }
+    
+    // MARK: - Helpers
+    
+    private func setup() {
+        // TODO: Set Image
+        titleLabel.text = monument.title
+        streetLabel.text = monument.street
+        textLabel.text = monument.text
+    }
+    
+    private func openMonumentInMaps() {
+        let coordinate = monument.toDenkmal().coordinate
+        let placemark = MKPlacemark(coordinate: coordinate)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = monument.title
+        mapItem.openInMaps(launchOptions: nil)
+    }
+    
 }
 
 // MARK: - UIScrollViewDelegate
