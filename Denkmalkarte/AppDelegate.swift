7e67844f7c95355668b408a0f0b7a3c175b2fc19
‘Florian <fhaeusler87@googlemail.com>
2018-12-19 14:11:52 +0100

    siri bugfix

--- Denkmalkarte/AppDelegate.swift
+++ Denkmalkarte/AppDelegate.swift
@@ -11,7 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
 
         // init
-        initialize()
+        //initialize()
 
         print(userActivity.activityType)
         switch userActivity.activityType {
