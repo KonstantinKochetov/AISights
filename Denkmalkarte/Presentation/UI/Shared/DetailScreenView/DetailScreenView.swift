55f7522b96ee3963b55f199745422ad1a2c63e0d
C <c@example.com>
2019-01-06 18:12:11 +0100

    fix

--- Denkmalkarte/Presentation/UI/Shared/DetailScreenView/DetailScreenView.swift
+++ Denkmalkarte/Presentation/UI/Shared/DetailScreenView/DetailScreenView.swift
@@ -9,51 +9,51 @@ public class DetailScreenView: UIViewController, DetailScreenViewProtocol {
     @IBOutlet private var titleLabel: UILabel!
     @IBOutlet private var streetLabel: UILabel!
     @IBOutlet private var textLabel: UILabel!
-    
+
     public override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
     var presenter: DetailScreenPresenterProtocol?
     var monument: RealmDenkmal?
-    
+
     public override func viewDidLoad() {
         setup()
     }
-    
+
     // MARK: Actions
-    
     @IBAction func dismissButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func directionsButtonTapped(_ sender: Any) {
         openMonumentInMaps()
     }
-    
+
     // MARK: - Helpers
-    
     private func setup() {
         // TODO: Set Image
-        titleLabel.text = monument.title
-        streetLabel.text = monument.street
-        textLabel.text = monument.text
+        if let monument = monument {
+            titleLabel.text = monument.title
+            streetLabel.text = monument.street
+            textLabel.text = monument.text
+        }
     }
-    
+
     private func openMonumentInMaps() {
-        let coordinate = monument.toDenkmal().coordinate
-        let placemark = MKPlacemark(coordinate: coordinate)
-        let mapItem = MKMapItem(placemark: placemark)
-        mapItem.name = monument.title
-        mapItem.openInMaps(launchOptions: nil)
+        if let monument = monument {
+            let coordinate = monument.toDenkmal().coordinate
+            let placemark = MKPlacemark(coordinate: coordinate)
+            let mapItem = MKMapItem(placemark: placemark)
+            mapItem.name = monument.title
+            mapItem.openInMaps(launchOptions: nil)
+        }
     }
-    
 }
 
 // MARK: - UIScrollViewDelegate
 
 extension DetailScreenView: UIScrollViewDelegate {
-    
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView == self.scrollView {
             let yOffset = scrollView.contentOffset.y
@@ -62,5 +62,4 @@ extension DetailScreenView: UIScrollViewDelegate {
             if yOffset <= 0 { imageView.transform = transform }
         }
     }
-    
 }
